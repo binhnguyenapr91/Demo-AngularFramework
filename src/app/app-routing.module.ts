6e5e7dd522d498688e7d0fc4b68dd8d288c4cd75
binhnguyenapr91 <binhnguyen.apr91@gmail.com>
2020-07-09 22:40:46 +0700

    finish adding router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {FontSizeEditorComponent} from './font-size-editor/font-size-editor.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'font-size-editor', component: FontSizeEditorComponent
+  },
+  {
+    path: '', redirectTo: '\font-size-editor', pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
